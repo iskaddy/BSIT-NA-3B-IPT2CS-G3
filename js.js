c4f73033844f0e747fbf241cdfb5cc56cbe00cc2
KiannLavarias <159234278+KiannLavarias@users.noreply.github.com>
2024-03-08 20:42:15 +0800

    JS FILE
    FINA JS FILE

--- js.js
+++ js.js
@@ -1,8 +1,6 @@
-// Select the button element
+
 const toggleModeBtn = document.getElementById('toggle-mode-btn');
 
-// Add click event listener to the button
 toggleModeBtn.addEventListener('click', () => {
-    // Toggle the body class to switch between light and dark mode
     document.body.classList.toggle('light-mode');
 });
